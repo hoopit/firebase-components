8f1c810513e3036d69b9ef1e0b3814e8628e51ca
Lars-Kristian Dahl <lars-kristian@hoopit.io>
2019-02-15 11:12:02 +0100

    Refactor and simplify NetworkStateObserver

---
+++ ui/src/main/java/io/hoopit/android/ui/widgets/NetworkStateObserver.kt
@@ -0,0 +1,36 @@
+package io.hoopit.android.ui.widgets
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import io.hoopit.android.ui.NetworkState
+
+abstract class NetworkStateObserver(
+    private val networkState: LiveData<NetworkState>
+) : Observer<NetworkState> {
+
+    final override fun onChanged(it: NetworkState?) {
+        if (it == null) return
+        when (it.status) {
+            NetworkState.Status.SUCCESS -> {
+                networkState.removeObserver(this)
+                onSuccess(it)
+            }
+            NetworkState.Status.FAILED -> {
+                networkState.removeObserver(this)
+                onError(it)
+            }
+            NetworkState.Status.RUNNING -> {
+                onLoading(it)
+            }
+        }
+    }
+
+    open fun onSuccess(networkState: NetworkState) {}
+    open fun onError(networkState: NetworkState) {}
+    open fun onLoading(networkState: NetworkState) {}
+
+    fun observe(lifecycleOwner: LifecycleOwner) {
+        networkState.observe(lifecycleOwner, this)
+    }
+}
