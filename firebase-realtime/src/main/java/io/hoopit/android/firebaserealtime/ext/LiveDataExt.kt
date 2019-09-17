51e355587043574c83edb115b10926bbcb9c9ec2
Lars-Kristian Dahl <lars-kristian@hoopit.io>
2019-09-17 08:34:52 +0200

    Fix orderByChildProperty
    Clean up old livedata subscriptions

--- firebase-realtime/src/main/java/io/hoopit/android/firebaserealtime/ext/LiveDataExt.kt
+++ firebase-realtime/src/main/java/io/hoopit/android/firebaserealtime/ext/LiveDataExt.kt
@@ -2,27 +2,51 @@ package io.hoopit.android.firebaserealtime.ext
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
+import io.hoopit.android.common.switchMap
 import java.util.TreeMap
 
 inline fun <T, K, P : Comparable<P>> LiveData<List<T>>.orderByChildProperty(
     crossinline childGetter: (T) -> LiveData<K?>,
-    crossinline propertyGetter: (K) -> P
-): MediatorLiveData<List<T>> {
-    val med = MediatorLiveData<List<T>>()
-    med.addSource(this) { list ->
-        if (list.isEmpty()) med.postValue(emptyList())
+    crossinline childPropertyGetter: (K) -> P
+): LiveData<List<T>> {
+    return this.switchMap { list ->
+        val liveData = MediatorLiveData<List<T>>()
+        if (list.isEmpty()) {
+            liveData.postValue(list)
+            return@switchMap liveData
+        }
         val map = TreeMap<P, T>()
         list.forEach { item ->
-            val asd = childGetter(item)
-            med.removeSource(asd)
-            med.addSource(asd) { subItem ->
+            val child = childGetter(item)
+            liveData.removeSource(child)
+            liveData.addSource(child) { subItem ->
                 subItem?.let {
                     map.values.remove(item)
-                    map[propertyGetter(it)] = item
-                    med.postValue(map.values.reversed())
+                    map[childPropertyGetter(it)] = item
+                    liveData.postValue(map.values.reversed())
                 }
             }
         }
+        return@switchMap liveData
     }
-    return med
+
+//    liveData.addSource(this) { list ->
+//        if (list.isEmpty()) {
+//            liveData.postValue(mediatorLiveData(emptyList()))
+//            return@addSource
+//        }
+//        val childLiveData = MediatorLiveData<List<T>>()
+//        liveData.postValue(childLiveData)
+//        list.forEach { item ->
+//            val child = childGetter(item)
+//            liveData.removeSource(child)
+//            childLiveData.addSource(child) { subItem ->
+//                subItem?.let {
+//                    map.values.remove(item)
+//                    map[childPropertyGetter(it)] = item
+//                    childLiveData.postValue(map.values.reversed())
+//                }
+//            }
+//        }
+//    }
 }
